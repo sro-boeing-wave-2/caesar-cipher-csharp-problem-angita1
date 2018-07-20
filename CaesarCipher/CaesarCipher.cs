5551aeacb4a682a1533d488686f602798d6bad7b
Yaiphabee <ksh.yaipha@gmail.com>
2018-07-20 14:56:36 +0530

    added the long version of caaser

--- CaesarCipher/CaesarCipher.cs
+++ CaesarCipher/CaesarCipher.cs
@@ -7,7 +7,49 @@ namespace CaesarCipher
     {
         public static string Rotate(string text, int shiftKey)
         {
+            string rotate = "";
+
+            foreach (char item in text)
+            {
+                if (!char.IsLetter(item))
+                {
+
+                    rotate += item;
+                }
+                else
+                {
+                    if (item >= 'a' && item <= 'z')
+                    {
+                        char c = (char)(item + shiftKey);
+                        if (c > 'z')
+                        {
+                            rotate += (char)(c - 26);
+                        }
+                        else
+                        {
+                            rotate += c;
+                        }
+
+                    }
+                    if ((item >= 'A') && (item <= 'Z'))
+                    {
+                        char d = (char)(item + shiftKey);
+                        if (d > 'Z')
+                        {
+                            rotate += (char)(d - 26);
+                        }
+                        else
+                        {
+                            rotate += d;
+                        }
+                    }
+                }              
+            }
+            return rotate;
             throw new NotImplementedException("You need to implement this function.");
         }
+           
+       
+
     }
 }
